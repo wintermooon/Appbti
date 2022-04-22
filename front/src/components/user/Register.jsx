47e3ea107f5282ffe7fef72aab6c9b8c34a4f35b
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-04-22 19:33:03 +0900

    [Create] : 회원가입 기능 구현 완료
    회원가입 예외 처리 어떻게 할지 논의해봐도 좋을 것 같다.
    
    [As-is]
    - name : 최소 2자리 이상
    - email : aaa@aa.com 메일 형식이 맞는지 확인
    - password: 최소 8자리 이상
    - passwordConfirm : 비밀번호 일치 여부 확인

---
+++ front/src/components/user/Register.jsx
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import * as Api from "../../api";
+
+const Register = function () {
+  const theme = createTheme();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [name, setName] = useState("");
+
+  //이메일이 abc@example.com 형태인지 확인
+  const validateEmail = (email) => {
+    return email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  };
+
+  // 이메일 형태 적합 여부 확인
+  const isEmailValid = validateEmail(email);
+  // 비밀번호가 8글자 이상인지 여부 확인.
+  const isPasswordValid = password.length >= 8;
+  // 비밀번호와 확인용 비밀번호가 일치 여부 확인
+  const isPasswordSame = password === passwordConfirm;
+  // 이름 2글자 이상인지 여부 확인
+  const isNameValid = name.length >= 2;
+
+  // 4개의 조건 모두 동시 만족 여부 확인
+  const isFormValid = isEmailValid && isPasswordValid && isPasswordSame && isNameValid;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // "user/register" 엔드포인트로 post요청함.
+      await Api.post("user/register", {
+        email,
+        password,
+        name,
+      });
+
+      // 로그인 페이지로 이동함.
+      navigate("/login");
+    } catch (err) {
+      console.log("회원가입에 실패하였습니다.", err);
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="on"
+                  name="name"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Name"
+                  autoFocus
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                {!isNameValid && (
+                  <p className="text-primary" id="change-text-sucess" style={{ fontSize: "12px", margin: "5px 0 0 0" }}>
+                    Name is too short (minimum is 2 characters)
+                  </p>
+                )}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="on"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+                {!isEmailValid && (
+                  <p className="text-primary" style={{ fontSize: "12px", margin: "5px 0 0 0" }}>
+                    Email is invalid.
+                  </p>
+                )}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="on"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                {!isPasswordValid && (
+                  <p className="text-primary" style={{ fontSize: "12px", margin: "5px 0 0 0" }}>
+                    Password is too short (minimum is 8 characters)
+                  </p>
+                )}
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="passwordConfirm"
+                  label="Password-Confirm"
+                  type="password"
+                  id="passwordConfirm"
+                  autoComplete="on"
+                  value={passwordConfirm}
+                  onChange={(e) => {
+                    setPasswordConfirm(e.target.value);
+                  }}
+                />
+                {!isPasswordSame && (
+                  <p className="text-primary" id="change-text-sucess" style={{ fontSize: "12px", margin: "5px 0 0 0" }}>
+                    Passwords dose not match.
+                  </p>
+                )}
+              </Grid>
+            </Grid>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} disabled={!isFormValid}>
+              Register
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link className="loginBtn" onClick={() => navigate("/login")} variant="body2">
+                  Already have an account?
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default Register;
