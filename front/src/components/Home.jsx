22b311bf2a83c23e16524e95a51df0aa8fef7a5a
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-04-24 06:04:18 +0900

    [Delete] : yarn.lock 파일 삭제
    프론트는 ESLint, Prettier 관련 이슈로npm으로 해야 작동해서 파일 삭제

---
+++ front/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Main from "./img/Main.png";
+
+const Home = function () {
+  return (
+    <div>
+      <img src={Main} alt="" style={{ width: "100%" }} />
+    </div>
+  );
+};
+
+export default Home;
