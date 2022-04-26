650fee67a193858d0460f50b25988657c2e50cea
정예승 <jeong-yeseung@jeong-yeseung-ui-MacBookAir.local>
2022-04-26 17:12:29 +0900

    [Create]: Question mvp

--- back/src/app.js
+++ back/src/app.js
@@ -4,6 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import { userAuthRouter } from './routers/userRouter';
 import { freeboardRouter } from './routers/freeboardRouter';
 import { commentRouter } from './routers/commentRouter';
+import { questionRouter } from './routers/questionRouter';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import swaggerFile from './swagger/swagger-output.json';
 
@@ -29,15 +30,11 @@ app.get('/', (req, res) => {
 app.use(userAuthRouter);
 app.use(freeboardRouter);
 app.use(commentRouter);
+app.use(questionRouter);
 
-console.log(swaggerFile)
+console.log(swaggerFile);
 //swagger
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerFile, { explorer: true })
-
-);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile, { explorer: true }));
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
