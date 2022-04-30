42627ca166500ee1b1d52ae1fd70d30ae554c08b
YounJoo Seog <reina.seog@gmail.com>
2022-04-30 17:14:54 +0900

    [front][modify] 댓글창 변경(params error상태)

--- front/src/components/community/freeboard/FreeboardCommentForm.jsx
+++ front/src/components/community/freeboard/FreeboardCommentForm.jsx
@@ -10,10 +10,9 @@ function CommentAddForm({ setComments, user, postId }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user);
     try {
       if (window.confirm(`댓글을 남기시겠습니까?`)) {
-        const { data: createdComment } = await Api.post(`freeboard/comment`, {
+        const { data: createdComment } = await Api.post(`freeboards/comments`, {
           board_id: postId,
           user_id: user._id,
           name: user.name,
@@ -30,7 +29,7 @@ function CommentAddForm({ setComments, user, postId }) {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <h3>{user.name}</h3>
+        {/* <h3>{user.name}</h3> */}
         <textarea id="FreeboardComment" placeholder="바르고 고운 댓글을 달아주세요 *^~^*" onChange={(e) => handleCommentValue("content", e.target.value)} value={tempComment.content} />
         <button type="submit">등록</button>
       </form>
