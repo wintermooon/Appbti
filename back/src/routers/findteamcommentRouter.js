d6fd9eb6f67b713f44e2f9e8e6db61bd20ab0209
yuhayan <cnessit@gmail.com>
2022-05-01 15:15:47 +0900

    [back][fix] findteam 공사(좋아요, query 등)

--- back/src/routers/findteamcommentRouter.js
+++ back/src/routers/findteamcommentRouter.js
@@ -6,19 +6,20 @@ import { findteamcommentService } from '../services/findteamcommentService';
 const findteamcommentRouter = Router();
 // ftcommentRouter.use(loginRequired);
 
-findteamcommentRouter.post('/findteams/comments', loginRequired, async (req, res, next) => {
+findteamcommentRouter.post('/findteamcomments', loginRequired, async (req, res, next) => {
   try {
     /*
      #swagger.tags = ['findteamcomment'] 
      #swagger.summary = '팀 찾기 게시글에 댓글 생성' 
      #swagger.security = [{ "bearerAuth": [] }]
     */
-    const { board_id, user_id, name, content } = req.body;
+    const userId = req.currentUserId;
+    const post_id = req.query.post_id;
+    const { content } = req.body;
 
     const newComment = await findteamcommentService.addComment({
-      board_id,
-      user_id,
-      name,
+      userId,
+      post_id,
       content,
     });
 
@@ -32,45 +33,22 @@ findteamcommentRouter.post('/findteams/comments', loginRequired, async (req, res
   }
 });
 
-// 특정 댓글 조회 API
-findteamcommentRouter.get('/findteams/comments/:id', loginRequired, async (req, res, next) => {
-  try {
-    /*
-     #swagger.tags = ['findteamcomment'] 
-     #swagger.summary = '댓글 id에 해당하는 댓글 가져오기' 
-     #swagger.security = [{ "bearerAuth": [] }]
-    */
-    const comment_id = req.params.id;
-    const currentComment = await findteamcommentService.getComment({ comment_id });
-
-    if (currentComment.errorMessage) {
-      throw new Error(currentComment.errorMessage);
-    }
-
-    res.status(200).send(currentComment);
-  } catch (error) {
-    next(error);
-  }
-});
 
 // 특정 댓글 수정 API
-findteamcommentRouter.put('/findteams/comments/:id', loginRequired, async (req, res, next) => {
+findteamcommentRouter.put('/findteamcomments/:id', loginRequired, async (req, res, next) => {
   try {
     /*
      #swagger.tags = ['findteamcomment'] 
      #swagger.summary = '댓글 수정하기' 
      #swagger.security = [{ "bearerAuth": [] }]
     */
+    const userId = req.currentUserId;
     const comment_id = req.params.id;
-    const board_id = req.body.board_id ?? null;
-    const user_id = req.body.user_id ?? null;
-    const name = req.body.name ?? null;
     const content = req.body.content ?? null;
-    const created_at = req.body.created_at ?? null;
 
-    const toUpdate = { board_id, user_id, name, content, created_at };
+    const toUpdate = { content };
 
-    const updatedComment = await findteamcommentService.setComment({ comment_id, toUpdate });
+    const updatedComment = await findteamcommentService.setComment({ userId, comment_id, toUpdate });
 
     if (updatedComment.errorMessage) {
       throw new Error(updatedComment.errorMessage);
@@ -82,7 +60,7 @@ findteamcommentRouter.put('/findteams/comments/:id', loginRequired, async (req, 
   }
 });
 
-findteamcommentRouter.delete('/findteams/comments/:id', loginRequired, async (req, res, next) => {
+findteamcommentRouter.delete('/findteamcomments/:id', loginRequired, async (req, res, next) => {
   try {
     /*
      #swagger.tags = ['findteamcomment'] 
@@ -102,15 +80,15 @@ findteamcommentRouter.delete('/findteams/comments/:id', loginRequired, async (re
   }
 });
 
-findteamcommentRouter.get('/findteams/usercommentlist/:user_id', loginRequired, async (req, res, next) => {
+findteamcommentRouter.get('/findteamcomments/:userId', loginRequired, async (req, res, next) => {
   try {
     /*
      #swagger.tags = ['findteamcomment'] 
      #swagger.summary = '유저 id가 작성한 댓글 목록 가져오기' 
      #swagger.security = [{ "bearerAuth": [] }]
     */
-    const user_id = req.params.user_id;
-    const comments = await findteamcommentService.getCommentsById({ user_id });
+    const userId = req.params.userId;
+    const comments = await findteamcommentService.getCommentsById({ userId });
 
     res.status(200).send(comments);
   } catch (error) {
@@ -118,15 +96,15 @@ findteamcommentRouter.get('/findteams/usercommentlist/:user_id', loginRequired, 
   }
 });
 
-findteamcommentRouter.get('/findteams/boardcommentlist/:board_id', loginRequired, async (req, res, next) => {
+findteamcommentRouter.get('/findteamcomments', loginRequired, async (req, res, next) => {
   try {
     /*
      #swagger.tags = ['findteamcomment'] 
      #swagger.summary = '게시글의 모든 댓글 목록 가져오기' 
      #swagger.security = [{ "bearerAuth": [] }]
     */
-    const board_id = req.params.board_id;
-    const comments = await findteamcommentService.getCommentsByBoardId({ board_id });
+    const post_id = req.query.post_id;
+    const comments = await findteamcommentService.getComments({ post_id });
 
     res.status(200).send(comments);
   } catch (error) {
