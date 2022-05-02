a8ca78528187b6db36997a8664b0bc965fd147cc
YounJoo Seog <reina.seog@gmail.com>
2022-05-02 21:20:38 +0900

    [front][create]appbti create

---
+++ front/src/components/appbtitest/AppbtiTest.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const AppbtiTest() {
+    return 
+    <section>
+         <Header />
+        <span>"안녕"</span>
+    </section>;
+}
+
+export default AppbtiTest;
